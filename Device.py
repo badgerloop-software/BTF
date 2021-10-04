98bb4381acda81ca24191a759110ba08976cbcf2
Sam O <sorlan@wisc.edu>
2021-10-04 09:41:06 -0500

    Add more comments

--- Device.py
+++ Device.py
@@ -1,2 +1,10 @@
 # This file represents a device that the Raspberry Pi will simulate.
-# TODO implement
+# Instance Fields:
+#   SCP (serial communication protocol)
+#
+# Instance Functions:
+#   Input(): Read from registers
+#
+#   Output(): Write to registers
+
+# TODO Implement
