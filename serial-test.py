c6139d1de73ddc6c3d4c63a515ba3c474845a474
bquan0 <powerbowen@gmail.com>
2021-10-26 17:59:03 -0500

    update
    Signed-off-by: bquan0 <powerbowen@gmail.com>

--- serial-test.py
+++ serial-test.py
@@ -1,14 +1,18 @@
 import serial
 
 ser = serial.Serial()
-ser.port = "/dev/ttyS0"
-ser.baudrate = 115200
+ser.port = "/dev/serial0"
+ser.baudrate = 9600
 
 if not ser.is_open:
     ser.open()
+ser.reset_input_buffer()
 
 data = ser.read(8)
 print(data)
+
+str = data.decode("utf-8")
+print (str)
 
 ser.close()
 print(ser.is_open)
