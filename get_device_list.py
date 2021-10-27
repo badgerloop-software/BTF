1ca375d6040bf146ca066893c2020c1dabbd8376
Samuel Orlan <sorlan@wisc.edu>
2021-10-26 19:57:16 -0500

    rename main to get_device_list, and have function return list of devices generated

--- get_device_list.py
+++ get_device_list.py
@@ -1,8 +1,11 @@
-# Driver function for setting up simulated Devices
 from modules.Register import Register
 
-def main():
-    # Create list of Devices
+def get_device_list():
+    """Forms a device list in which devices can be indexed by the device's name.
+        
+        Returns: A list of mock devices to use for testing
+    """
+    # Create devices
     device_0 = {
         0x00: Register(0xFF, 8, True),
         0x01: Register(0x52, 8, True),
@@ -17,7 +20,7 @@ def main():
         0x03: Register(0x11, 8, False)
     }
 
-    # Can pass this device_list to wherever tests are ran
+    # device_list can be passed to wherever tests are ran
     device_list = {
         "d0": device_0,
         "d1": device_1
@@ -27,7 +30,9 @@ def main():
     for device_name in device_list:
         for reg_address in device_list[device_name]:
             print(device_list[device_name][reg_address])
+    
+    return device_list
 
 
 if __name__ == "__main__":
-    main()
+    get_device_list()
