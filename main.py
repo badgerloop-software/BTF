549b7992046236ddd7100c003f9006a162c75f66
Samuel Orlan <sorlan@wisc.edu>
2021-10-26 19:20:13 -0500

    refactor device list generation into main

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-# Driver function for testing stuff
-from configparser import ConfigParser
-import json
+# Driver function for setting up simulated Devices
+from modules.Register import Register
+
+def main():
+    # Create list of Devices
+    device_0 = {
+        0x00: Register(0xFF, 8, True),
+        0x01: Register(0x52, 8, True),
+        0x02: Register(0x00, 8, False),     
+        0x03: Register(0x11, 8, False)
+    }
+
+    device_1 = {
+        0x00: Register(0xFF, 8, True),
+        0x01: Register(0x52, 8, True),
+        0x02: Register(0x00, 8, False),     
+        0x03: Register(0x11, 8, False)
+    }
+
+    # Can pass this device_list to wherever tests are ran
+    device_list = {
+        "d0": device_0,
+        "d1": device_1
+    }
+
+    # Debug print all registers accessible from the device_list
+    for device_name in device_list:
+        for reg_address in device_list[device_name]:
+            print(device_list[device_name][reg_address])
 
 
 if __name__ == "__main__":
-    #Read config.ini file
-    config_object = ConfigParser()
-    config_object.read("config.ini")
-
-    # Grab device list   
-    device_list = config_object.get("DEVICE_LIST", "value")
-    # TODO This only sees device_list as a string. Is using ConfigParser necessary?
-    print(device_list)
+    main()
