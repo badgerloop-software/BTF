081ada0cda08fa39ea36be60310b3b79893012e4
bquan0 <powerbowen@gmail.com>
2021-10-31 12:04:43 -0500

    i2c communication with the devices (read and write)

--- i2c.py
+++ i2c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from os import read
+from modules.Register import Register
+from get_device_list import device_list
 import time
 import pigpio
 import sys
@@ -8,18 +10,18 @@ import serialtest
 
 I2C_ADDR=0x24
 pi = pigpio.pi()
-register = [None, None]
+device = None
 
 def i2c(id, tick):
    s, b, d = pi.bsc_i2c(I2C_ADDR)
    if b > 1:
        print("Received")
-       register[d[0]] = bytearray([d[1]])
+       device[d[0]].write(d[1])
        print("put {register[d[0]]} in reg")
 
    elif b == 1:
        print("Request to read")
-       pi.bsc_i2c(I2C_ADDR, register[d[0]])
+       pi.bsc_i2c(I2C_ADDR, device[d[0]].data)
 
 if not pi.connected:
     exit()
@@ -32,9 +34,12 @@ if len(sys.argv) > 1:
 # Respond to BSC slave activity
 e = pi.event_callback(pigpio.EVENT_BSC, i2c)
 pi.bsc_i2c(I2C_ADDR) # Configure BSC as I2C slave
+
 serialtest.openSerial()
 data = serialtest.readBytes()
 while data != "stop":
+    if data in device_list:
+        device = device_list[data]
     time.sleep(1)
     data = serialtest.readBytes()
 
