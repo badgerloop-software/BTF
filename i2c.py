cb237ba16dfca0ff936b369ee14b5821a22ec859
bquan0 <91704174+bquan0@users.noreply.github.com>
2021-11-14 11:49:26 -0600

    fix i2c communication (#2)
    * create serial file
    
    Signed-off-by: bquan0 <powerbowen@gmail.com>
    
    * testing
    
    Signed-off-by: bquan0 <powerbowen@gmail.com>
    
    * another test
    
    Signed-off-by: bquan0 <powerbowen@gmail.com>
    
    * update
    
    Signed-off-by: bquan0 <powerbowen@gmail.com>
    
    * first API test
    
    * rough draft for reading serial connection
    
    * sending stop string from beaglebone to pi
    
    * sending stop string from beaglebone to pi part 2
    
    * sending stop string from beaglebone to pi part 3
    
    * sending stop string from beaglebone to pi part 4
    
    * sending stop string from beaglebone to pi part 5
    
    * sending stop string from beaglebone to pi part 6
    
    * fixup serial
    
    * i2c communication with the devices (read and write)
    
    * working product
    
    * removed commented out code
    
    Co-authored-by: Marco Kurzynski <mkurzynski@wisc.edu>

--- i2c.py
+++ i2c.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
+from os import read
+from modules.Register import Register
+from get_device_list import get_device_list 
 import time
 import pigpio
 import sys
+import serialtest
 
 I2C_ADDR=0x24
+device_list = get_device_list();
 pi = pigpio.pi()
-register = [None, None]
+device = device_list["mcp23017"]
 
 def i2c(id, tick):
    s, b, d = pi.bsc_i2c(I2C_ADDR)
    if b > 1:
-       print("Received")
-       register[d[0]] = bytearray([d[1]])
-       print(f"put {register[d[0]]} in reg")
+       device[d[0]].write(d[1])
+       print(f"\nput {hex(d[1])} in reg {hex(d[0])}\nnew value: {hex(device[d[0]].data)}\n")
 
    elif b == 1:
-       print("Request to read")
-       pi.bsc_i2c(I2C_ADDR, register[d[0]])
+       print(f"Request to read register {hex(d[0])}")
+       pi.bsc_i2c(I2C_ADDR, device[d[0]].data.to_bytes(1,byteorder='big', signed=False))
+       print(device[d[0]].data.to_bytes(1,byteorder='big', signed=False))
 
 if not pi.connected:
     exit()
@@ -31,7 +36,16 @@ if len(sys.argv) > 1:
 e = pi.event_callback(pigpio.EVENT_BSC, i2c)
 pi.bsc_i2c(I2C_ADDR) # Configure BSC as I2C slave
 
-time.sleep(600)
+serialtest.openSerial()
+data = serialtest.readBytes()
+while data != "stop":
+    if data in device_list:
+        device = device_list[data]
+    time.sleep(1)
+    data = serialtest.readBytes()
+
+serialtest.closeSerial()
+
 e.cancel()
 
 pi.bsc_i2c(0) # Disable BSC peripheral
---
+++ serialtest.py
@@ -0,0 +1,22 @@
+import serial
+    
+ser = serial.Serial()
+ser.port = "/dev/serial0"
+ser.baudrate = 9600
+
+def openSerial():
+    if not ser.is_open:
+        ser.open()
+        return ser.is_open
+
+def readBytes():
+    ser.reset_input_buffer()
+    while ser.in_waiting == 0:
+        pass
+    data = ser.read(ser.in_waiting)
+    str = data.decode("utf-8")
+    return str.strip()
+
+def closeSerial():
+    ser.close()
+    return ser.is_open
