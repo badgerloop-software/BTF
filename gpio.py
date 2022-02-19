629b7513921a3c1aa6d9033c9e82ddc3a1fe1cf1
Noah Kurszewski <nkurszewski@wisc.edu>
2022-02-18 19:41:45 -0600

    Created 2 functions to test the interaction between the gpio pins on the pi and BBB

--- gpio.py
+++ gpio.py
@@ -1,5 +1,9 @@
 import pigpio
 
+# Define pins
+pinR = 15
+pinW = 16
+
 ## Write functions to handle the following gpio cases
 
 ## Use pigpio library for interfacing with gpio pins
@@ -7,22 +11,13 @@ import pigpio
 
 ## Case 1: 
 # pin on BBB is set to 0 and 1 and needs to be validated by the Pi
-def pi_read(pi):
+def pi_read(pi, expected):
     pi.set_mode(pinR, pigpio.INPUT)
-    # pi reads BBB pin
-
-    # pi outputs read value & caller verifies result
-    # OR function compares BBB value and read value
+    # return if read value matches expected
+    return(pi.read(pinR) == expected)
 
 ## Case 2:
 # pin on pi to be set to 0 and 1 by BBB and will be validated by the BBB
-def pi_write(pi):
+def pi_write(pi, level):
     pi.set_mode(pinW, pigpio.OUTPUT)
-
-    # high test
-    pi.write(pinW, high)
-        #BBB checks gpio value
-    
-    #low test
-    pi.write(pinW, low)
-        #BBB checks gpio value
+    pi.write(pinW, level)   # set level
