0f11593956576d063d9489fe366b84dd4b28a8c6
ykalra <91573152+ykalra@users.noreply.github.com>
2022-02-22 16:47:45 -0600

    added tca6416 device (#4)

--- get_device_list.py
+++ get_device_list.py
@@ -13,9 +13,21 @@ def get_device_list():
         0x13: Register(0x00, 8, True)
     }
 
+    tca6416 = {
+        0x00: Register(0xFF, 8, False),
+        0x01: Register(0xFF, 8, False),
+        0x02: Register(0xFF, 8, True),     
+        0x03: Register(0xFF, 8, True),
+        0x04: Register(0x00, 8, True),
+        0x05: Register(0x00, 8, True),
+        0x06: Register(0xFF, 8, True),     
+        0x07: Register(0xFF, 8, True),
+    }
+
     # device_list can be passed to wherever tests are ran
     device_list = {
-        "mcp23017": mcp23017
+        "mcp23017": mcp23017,
+        "tca6416": tca6416
     }
 
     # Debug print all registers accessible from the device_list
