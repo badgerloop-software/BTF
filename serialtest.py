e2ffd6aa35bd1780b5565c09c282f58452e6a6a5
Ben Everson <bennye3797@gmail.com>
2022-02-27 13:19:03 -0600

    fix serial reads

--- serialtest.py
+++ serialtest.py
@@ -10,10 +10,7 @@ def openSerial():
         return ser.is_open
 
 def readBytes():
-    ser.reset_input_buffer()
-    while ser.in_waiting == 0:
-        pass
-    data = ser.read(ser.in_waiting)
+    data = ser.readline()
     str = data.decode("utf-8")
     return str.strip()
 
