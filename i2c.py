e5de6c492f8a8a5a378ab5f0b7604c4b1b85dc04
Marco Kurzynski <marcokurzynski@icloud.com>
2022-03-18 02:05:53 +0000

    works for ina

--- i2c.py
+++ i2c.py
@@ -11,7 +11,7 @@ import serialtest
 I2C_ADDR=0x24
 device_list = get_device_list();
 pi = pigpio.pi()
-device = device_list["mcp23017"]
+device = device_list["ina3221"]
 
 def i2c(id, tick):
    s, b, d = pi.bsc_i2c(I2C_ADDR)
@@ -21,8 +21,7 @@ def i2c(id, tick):
 
    elif b == 1:
        print(f"Request to read register {hex(d[0])}")
-       pi.bsc_i2c(I2C_ADDR, device[d[0]].data.to_bytes(1,byteorder='big', signed=False))
-       print(device[d[0]].data.to_bytes(1,byteorder='big', signed=False))
+       pi.bsc_i2c(I2C_ADDR, device[d[0]].data.to_bytes(device[d[0]].size//8,byteorder='little', signed=False))
 
 if not pi.connected:
     exit()
@@ -36,16 +35,17 @@ if len(sys.argv) > 1:
 e = pi.event_callback(pigpio.EVENT_BSC, i2c)
 pi.bsc_i2c(I2C_ADDR) # Configure BSC as I2C slave
 
-serialtest.openSerial()
-data = serialtest.readBytes()
-while data != "stop":
-    if data in device_list:
-        device = device_list[data]
-    time.sleep(1)
-    data = serialtest.readBytes()
+#serialtest.openSerial()
+#data = serialtest.readBytes()
+#while data != "stop":
+#    if data in device_list:
+#        device = device_list[data]
+#    time.sleep(1)
+#    data = serialtest.readBytes()
+#
+#serialtest.closeSerial()
 
-serialtest.closeSerial()
-
+time.sleep(36000)
 e.cancel()
 
 pi.bsc_i2c(0) # Disable BSC peripheral
