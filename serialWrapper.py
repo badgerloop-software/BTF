92228271fad290bb226bef7397571551326162d6
James <jamesvollmer02@gmail.com>
2023-10-04 22:47:48 -0500

    TODO Add wait and pyyaml

---
+++ serialWrapper.py
@@ -0,0 +1,24 @@
+# this file is a copy of serialtest.py from the old BTF repo. 
+import serial
+    
+ser = serial.Serial()
+ser.port = "/dev/ttyACM0"
+ser.baudrate = 9600
+
+def openSerial():
+    if not ser.is_open:
+        ser.open()
+        return ser.is_open
+
+def readBytes():
+    data = ser.readline()
+    str = data.decode("utf-8")
+    return str.strip()
+
+def writeString(str):
+    data = str.encode("utf-8")
+    ser.write(data)
+
+def closeSerial():
+    ser.close()
+    return ser.is_open
--- testRunner.py
+++ testRunner.py
@@ -1,10 +1,29 @@
 import sys
+import time
+import yaml
+import serialWrapper
 
+serialWrapper.openSerial()
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('You must specify the paths to the configuration files as arguments\n\nUsage: python3 testRunner.py </path/to/testConfig.yml> </path/to/setupConfig.yml>')
         sys.exit(1)
+    # TODO Verify that files passed are yaml files (and are in the correct order)
     
     print("Hello World!")
+    for i in range(20):
+        if i == 15:
+            serialWrapper.writeString("Start now")
+            #if i == 16:
+            print(serialWrapper.readBytes())
+        print(f"{i}")
+        time.sleep(1)
+    with open(f'{sys.argv[1]}', 'r') as testFile, open(f'{sys.argv[2]}', 'r') as setupFile:
+        testCfg = yaml.safe_load(testFile)
+        setupCfg = yaml.safe_load(setupFile)
+        print(testCfg)
+        print(setupCfg)
+    print("Goodbye World!")
+    serialWrapper.closeSerial()
     sys.exit(0)
